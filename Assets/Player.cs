94e9ded9bf1866b25e89316d46adda6730792cad
Kayakaga <baijidevelop@gmail.com>
2021-09-04 10:48:53 +0300

    Movement + Rotation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //movement
+    Rigidbody2D rb;
+    [SerializeField]int speed;
+    //movement
+    float mPosX;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        mPosX = Input.GetAxis("Mouse X");
+        Debug.Log(mPosX);
+        transform.Rotate(new Vector3(0, 0, mPosX));
+        Movement();
+    }
+    void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
     }
 }

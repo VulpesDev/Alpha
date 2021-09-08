f29a7e5386e4517c97fd4ff3c39b240befb03421
Kayakaga <baijidevelop@gmail.com>
2021-09-08 14:21:46 +0300

    Settings not working when built

--- Assets/Scripts/Management/Management.cs
+++ Assets/Scripts/Management/Management.cs
@@ -21,7 +21,7 @@ public class Management : MonoBehaviour
     }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.M))
         {
             if (!menuIsOpen)
             {
